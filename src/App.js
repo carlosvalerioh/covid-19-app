582b4fe848a7e70cbab4730484115f8f7841e413
Carlos Valerio <cvalerio778@gmail.com>
2020-04-07 00:00:40 -0500

    Casi terminada

--- src/App.js
+++ src/App.js
@@ -1,27 +1,38 @@
 import React from "react";
 
 import { Cards, Chart, CountryPicker } from './components';
-import styles from './App.module.css'
 import { fetchData } from './api';
+import styles from './App.module.css'
+import image from './images/image.png';
+import { style } from "@material-ui/system";
 
 class App extends React.Component {
   state = {
     data: {},
-}
+    country: '',
+  }
 
-  async componentDidMount () {
-    const fetchedData = await fetchData();
+  async componentDidMount() {
+    const data = await fetchData();
 
-    this.setState({ data: fetchedData });
-    
+    this.setState({ data });
+  }
+
+  handleCountryChange = async (country) => {
+    const data = await fetchData(country);
+
+    this.setState({ data, country: country });
   }
   render() {
-    const { data } = this.state;
+    const { data, country } = this.state;
+
     return (
       <div className={styles.container}>
+      <img className={styles.image} src={image} alt="COVID-19" />
+      <div className={style.letra} > <h4> Por @carlosvalerioh  </h4></div>
         <Cards data={data} />
-        <CountryPicker/>
-        <Chart/>
+        <CountryPicker handleCountryChange={this.handleCountryChange} />
+        <Chart data={data} country={country} />
       </div>
     );
   }
