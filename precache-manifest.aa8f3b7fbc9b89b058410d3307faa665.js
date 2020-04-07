b44f33d03bb03f7ee8fea45c91aca071b402cdd4
Carlos Valerio <cvalerio778@gmail.com>
2020-04-07 18:57:46 -0500

    Updates

---
+++ precache-manifest.aa8f3b7fbc9b89b058410d3307faa665.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a04d6dc83f868a1dfb97fdf6ad21ac82",
+    "url": "/covid-19-app/index.html"
+  },
+  {
+    "revision": "103cacde9520dc2a518d",
+    "url": "/covid-19-app/static/css/main.d03bfe86.chunk.css"
+  },
+  {
+    "revision": "67a875af6293c3d732c1",
+    "url": "/covid-19-app/static/js/2.e1c5162f.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-19-app/static/js/2.e1c5162f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "103cacde9520dc2a518d",
+    "url": "/covid-19-app/static/js/main.18fe2a04.chunk.js"
+  },
+  {
+    "revision": "f56b1214a80aba04620b",
+    "url": "/covid-19-app/static/js/runtime-main.79476435.js"
+  }
+]);
